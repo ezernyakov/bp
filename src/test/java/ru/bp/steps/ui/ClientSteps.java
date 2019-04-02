cb00b954af50296e9bc2bf8e9b772e671276f265
Zernyakov Evgeniy <e.zernyakov@crystals.ru>
2019-04-02 19:27:06 +0300

    добавил основные степы для авторизации и работы с клиентом + фича на авторизацию

--- src/test/java/ru/bp/steps/ui/ClientSteps.java
+++ src/test/java/ru/bp/steps/ui/ClientSteps.java
@@ -1,4 +1,40 @@
 package ru.bp.steps.ui;
 
+import com.codeborne.selenide.Selenide;
+
+import cucumber.api.java.en.Given;
+import java.util.List;
+import ru.bp.context.Context;
+import ru.bp.pages.ClientListPage;
+import ru.bp.pages.ClientPage;
+import ru.bp.stub.server.entity.ClientEntity;
+
 public class ClientSteps {
+
+    private Context context;
+    private ClientListPage clientListPage = Selenide.page(ClientListPage.class);
+
+    public ClientSteps(Context context) {
+        this.context = context;
+    }
+
+    @Given("^на сервере нет клиентов с идентификатором:$")
+    public void clearCashiers(List<String> ids) throws IllegalAccessException {
+        for (String id : ids) {
+            ClientEntity client = new ClientEntity();
+            client.setIdNumber(id);
+
+            context.getCurrentServer().db().clients().delete(client).byPk();
+        }
+    }
+
+    @Given("^на сервере заведены клиенты$")
+    public void createUser(List<ClientsVO> clients) {
+        ServerManager manager = context.getCurrentServer().rmi(ServerManager.class, Jndi.CLIENTS.getName());
+        for (ClientsVO client : clients) {
+            manager.addCashier(null, client);
+        }
+    }
+
+
 }
--- src/test/java/ru/bp/steps/ui/OrderSteps.java
+++ src/test/java/ru/bp/steps/ui/OrderSteps.java
@@ -1,4 +1,6 @@
 package ru.bp.steps.ui;
 
 public class OrderSteps {
+
+    //TODO: реализовать шаги для тестов по работе со счетами
 }
