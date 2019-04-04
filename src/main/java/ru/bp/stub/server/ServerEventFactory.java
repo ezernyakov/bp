085db3573e1e8c816a03c1a496e263d327fc39f0
Zernyakov Evgeniy <e.zernyakov@crystals.ru>
2019-04-04 19:47:07 +0300

    backend тесты на авторизацию и работу с клиентами

--- src/main/java/ru/bp/stub/server/ServerEventFactory.java
+++ src/main/java/ru/bp/stub/server/ServerEventFactory.java
@@ -1,18 +1,55 @@
 package ru.bp.stub.server;
 
+import ru.bp.stub.server.entity.ClientEntity;
+import ru.bp.stub.server.payloads.FormClientCreateShowPld;
+
 /**
  * Фабрика событий сервера
  */
 
 public class ServerEventFactory {
 
+    public static ServerEvent userLogout() {
+        //stub, do something...
+        return null;
+    }
+
+    public static ServerEvent userLogin(String login, String password) {
+        //stub, do something...
+        return null;
+    }
+
+    public static ServerEventType formLoginShow() {
+        //stub, do something...
+        return null;
+    }
+
     public static ServerEventType formLogoutShow() {
         //stub, do something...
         return null;
     }
 
+    public static ServerEventType formErrorLoginShow() {
+        //stub, do something...
+        return null;
+    }
 
-    public static ServerEvent userLogout() {
+    public static ServerEventType formClientShow() {
+        //stub, do something...
+        return null;
+    }
+
+    public static ServerEvent formClientPut(FormClientCreateShowPld formClientCreateShowPld) {
+        //stub, do something...
+        return null;
+    }
+
+    public static ServerEvent formClientCreate(ClientEntity client) {
+        //stub, do something...
+        return null;
+    }
+
+    public static ServerEventType formClientCreateShow() {
         //stub, do something...
         return null;
     }
